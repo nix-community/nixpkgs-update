05133143f02d28c5b8eed9decec64599dad2733a
Ryan Mulligan <ryan@ryantm.com>
2018-03-22 18:12:21 -0700

    add tests for check-attrpath-version.sh and fix some bugs in it

--- check-attrpath-version.sh
+++ check-attrpath-version.sh
@@ -4,34 +4,47 @@ set -euxo pipefail
 ATTR_PATH=$1
 NEW_VERSION=$2
 
-ATTR_VERSION_PART=$(echo "$ATTR_PATH" | sed -n 's/^[^0-9]*\([0-9_]*\)$/\1/p')
+HAS_UNDERSCORES=$(echo "$ATTR_PATH" | grep "_")
 
-# If we don't find version numbers in the attr path, exit success.
-if [ -z "$ATTR_VERSION_PART" ]
+if [ -n "$HAS_UNDERSCORES" ]
 then
-    exit 0
-fi
+    ATTR_VERSION_PART=$(echo "$ATTR_PATH" | sed -n 's/^[^_]*_\([0-9_]*\)$/\1/p')
 
-# Check assuming version part has underscore separators
-ATTR_VERSION_PERIODS=$(echo "$ATTR_VERSION_PART" | sed -n 's/_/\./gp')
-
-if [ -n "$ATTR_VERSION_PERIODS" ]
-then
-   if [[ "$NEW_VERSION" == $ATTR_VERSION_PERIODS* ]]
-   then
-       exit 0
-   fi
-fi
-
-# Check assuming version part is the prefix of the version with dots
-# removed. For example, 91 => "9.1"
-NO_PERIOD_NEW_VERSION=$(echo "$NEW_VERSION" | sed -n 's/\.//gp')
-
-if [ -n "$NO_PERIOD_NEW_VERSION" ]
-then
-    if [[ "$NO_PERIOD_NEW_VERSION" == $ATTR_VERSION_PART* ]]
+    # If we don't find version numbers in the attr path, exit success.
+    if [ -z "$ATTR_VERSION_PART" ]
     then
         exit 0
+    fi
+
+    # Check assuming version part has underscore separators
+    ATTR_VERSION_PERIODS=$(echo "$ATTR_VERSION_PART" | sed -n 's/_/\./gp')
+
+    if [ -n "$ATTR_VERSION_PERIODS" ]
+    then
+        if [[ "$NEW_VERSION" == $ATTR_VERSION_PERIODS* ]]
+        then
+            exit 0
+        fi
+    fi
+else
+    ATTR_VERSION_PART=$(echo "$ATTR_PATH" | sed -n 's/^[^0-9]*\([0-9]*\)$/\1/p')
+
+    # If we don't find version numbers in the attr path, exit success.
+    if [ -z "$ATTR_VERSION_PART" ]
+    then
+        exit 0
+    fi
+
+    # Check assuming version part is the prefix of the version with dots
+    # removed. For example, 91 => "9.1"
+    NO_PERIOD_NEW_VERSION=$(echo "$NEW_VERSION" | sed -n 's/\.//gp')
+
+    if [ -n "$NO_PERIOD_NEW_VERSION" ]
+    then
+        if [[ "$NO_PERIOD_NEW_VERSION" == $ATTR_VERSION_PART* ]]
+        then
+            exit 0
+        fi
     fi
 fi
 
