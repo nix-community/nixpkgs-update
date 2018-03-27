6de0466d29cab2e5ff9587a4958726b152970025
Ryan Mulligan <ryan@ryantm.com>
2018-03-26 20:50:09 -0700

    add preamble to delete-merged.sh

--- delete-merged.sh
+++ delete-merged.sh
@@ -1,6 +1,14 @@
 #! /usr/bin/env bash
 set -euxo pipefail
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+GITHUB_TOKEN="$(cat "$SCRIPT_DIR"/github_token.txt)"
+export GITHUB_TOKEN
+
+# shellcheck source=setup-nixpkgs.sh
+source "$SCRIPT_DIR/setup-nixpkgs.sh"
+
 git fetch --prune origin
 git fetch --prune upstream
 
