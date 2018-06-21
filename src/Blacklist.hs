f9513412d1a738c67601a0b062d708eee44107b7
Ryan Mulligan <ryan@ryantm.com>
2018-06-21 06:01:32 -0700

    blacklist altcoins.bitcoin

--- src/Blacklist.hs
+++ src/Blacklist.hs
@@ -21,6 +21,7 @@ attrPath =
   [ (("lua" `T.isPrefixOf`), "Packages for lua are currently blacklisted.")
   , (("lxqt" `T.isPrefixOf`), "Packages for lxqt are currently blacklisted.")
   , (("altcoins.bitcoin-xt" `T.isPrefixOf`), "nix-prefetch-url has infinite redirect https://github.com/NixOS/nix/issues/2225 remove after Nix upgrade that includes https://github.com/NixOS/nix/commit/b920b908578d68c7c80f1c1e89c42784693e18d5.")
+  , (("altcoins.bitcoin" `T.isPrefixOf`), "@roconnor asked for a blacklist on this until something can be done with GPG signatures https://github.com/NixOS/nixpkgs/commit/77f3ac7b7638b33ab198330eaabbd6e0a2e751a9")
   ]
 
 packageName :: Text -> Maybe Text
