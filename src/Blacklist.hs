c255581c892aa7573a51d997dce209b71bafe5d2
Ryan Mulligan <ryan@ryantm.com>
2018-07-07 06:46:15 -0700

    simplify blacklists

--- src/Blacklist.hs
+++ src/Blacklist.hs
@@ -18,50 +18,51 @@ url =
 
 attrPath :: [(Text -> Bool, Text)]
 attrPath =
-  [ (("lua" `T.isPrefixOf`), "Packages for lua are currently blacklisted.")
-  , (("lxqt" `T.isPrefixOf`), "Packages for lxqt are currently blacklisted.")
-  , (("altcoins.bitcoin-xt" `T.isPrefixOf`), "nix-prefetch-url has infinite redirect https://github.com/NixOS/nix/issues/2225 remove after Nix upgrade that includes https://github.com/NixOS/nix/commit/b920b908578d68c7c80f1c1e89c42784693e18d5.")
-  , (("altcoins.bitcoin" `T.isPrefixOf`), "@roconnor asked for a blacklist on this until something can be done with GPG signatures https://github.com/NixOS/nixpkgs/commit/77f3ac7b7638b33ab198330eaabbd6e0a2e751a9")
+  [ prefix "lua" "Packages for lua are currently blacklisted."
+  , prefix "lxqt" "Packages for lxqt are currently blacklisted."
+  , prefix
+      "altcoins.bitcoin-xt"
+      "nix-prefetch-url has infinite redirect https://github.com/NixOS/nix/issues/2225 remove after Nix upgrade that includes https://github.com/NixOS/nix/commit/b920b908578d68c7c80f1c1e89c42784693e18d5."
+  , prefix
+      "altcoins.bitcoin"
+      "@roconnor asked for a blacklist on this until something can be done with GPG signatures https://github.com/NixOS/nixpkgs/commit/77f3ac7b7638b33ab198330eaabbd6e0a2e751a9"
   ]
 
 packageName :: Text -> Maybe Text
-packageName pn =
-  snd <$> find (\(isBlacklisted, _) -> isBlacklisted pn) nameList
+packageName pn = snd <$> find (\(isBlacklisted, _) -> isBlacklisted pn) nameList
 
 nameList :: [(Text -> Bool, Text)]
 nameList =
-  [ (("r-" `T.isPrefixOf`), "we don't know how to find the attrpath for these")
-  , (("jquery" `T.isInfixOf`), "this isn't a real package")
-  , (("google-cloud-sdk" `T.isInfixOf`), "complicated package")
-  , (("github-release" `T.isInfixOf`), "complicated package")
-  , (("fcitx" `T.isInfixOf`), "gets stuck in daemons")
-  , ( ("libxc" `T.isInfixOf`)
-    , "currently people don't want to update this https://github.com/NixOS/nixpkgs/pull/35821")
-  , (("perl" `T.isInfixOf`), "currently don't know how to update perl")
-  , (("python" `T.isInfixOf`), "currently don't know how to update python")
-  , (("cdrtools" `T.isInfixOf`), "We keep downgrading this by accident.")
-  , (("gst" `T.isInfixOf`), "gstreamer plugins are kept in lockstep.")
-  , (("electron" `T.isInfixOf`), "multi-platform srcs in file.")
-  , ( ("linux-headers" `T.isInfixOf`)
-    , "Not updated until many packages depend on it (part of stdenv).")
-  , ( ("mpich" `T.isInfixOf`)
-    , "Reported on repology.org as mischaracterized newest version")
-  , (("xfce" `T.isInfixOf`), "@volth asked to not update xfce")
-  , (("cmake-cursesUI-qt4UI" `T.isInfixOf`), "Derivation file is complicated")
-  , ( ("varnish" `T.isInfixOf`)
-    , "Temporary blacklist because of multiple versions and slow nixpkgs update")
-  , (("iana-etc" `T.isInfixOf`), "@mic92 takes care of this package")
-  , ( ("checkbashism" `T.isInfixOf`)
-    , "needs to be fixed, see https://github.com/NixOS/nixpkgs/pull/39552")
-  , ((== "isl"), "multi-version long building package")
-  , ((== "tokei"), "got stuck forever building with no CPU usage")
-  , ( ("qscintilla" `T.isInfixOf`)
-    , "https://github.com/ryantm/nixpkgs-update/issues/51")
-  , ((== "itstool"), "https://github.com/NixOS/nixpkgs/pull/41339")
-  , ( ("wire-desktop" `T.isInfixOf`)
-    , "nixpkgs-update cannot handle this derivation https://github.com/NixOS/nixpkgs/pull/42936#issuecomment-402282692")
-  , ( ("virtualbox" `T.isInfixOf`)
-    , "nixpkgs-update cannot handle updating the guest additions https://github.com/NixOS/nixpkgs/pull/42934")
+  [ prefix "r-" "we don't know how to find the attrpath for these"
+  , infixOf "jquery" "this isn't a real package"
+  , infixOf "google-cloud-sdk" "complicated package"
+  , infixOf "github-release" "complicated package"
+  , infixOf
+      "libxc"
+      "currently people don't want to update this https://github.com/NixOS/nixpkgs/pull/35821"
+  , infixOf "perl" "currently don't know how to update perl"
+  , infixOf "python" "currently don't know how to update python"
+  , infixOf "cdrtools" "We keep downgrading this by accident."
+  , infixOf "gst" "gstreamer plugins are kept in lockstep."
+  , infixOf "electron" "multi-platform srcs in file."
+  , infixOf
+      "linux-headers"
+      "Not updated until many packages depend on it (part of stdenv)."
+  , infixOf "xfce" "@volth asked to not update xfce"
+  , infixOf "cmake-cursesUI-qt4UI" "Derivation file is complicated"
+  , infixOf "iana-etc" "@mic92 takes care of this package"
+  , infixOf
+      "checkbashism"
+      "needs to be fixed, see https://github.com/NixOS/nixpkgs/pull/39552"
+  , eq "isl" "multi-version long building package"
+  , infixOf "qscintilla" "https://github.com/ryantm/nixpkgs-update/issues/51"
+  , eq "itstool" "https://github.com/NixOS/nixpkgs/pull/41339"
+  , eq
+      "wire-desktop"
+      "nixpkgs-update cannot handle this derivation https://github.com/NixOS/nixpkgs/pull/42936#issuecomment-402282692"
+  , infixOf
+      "virtualbox"
+      "nixpkgs-update cannot handle updating the guest additions https://github.com/NixOS/nixpkgs/pull/42934"
   ]
 
 content :: [(Text, Text)]
@@ -83,4 +84,19 @@ checkResult pn =
 
 checkResultList :: [(Text -> Bool, Text)]
 checkResultList =
-  [ (("busybox" `T.isInfixOf`), "- busybox result is not automatically checked, because some binaries kill the shell") ]
+  [ infixOf
+      "busybox"
+      "- busybox result is not automatically checked, because some binaries kill the shell"
+  , infixOf
+      "fcitx"
+      "- fcitx result is not automatically checked, because some binaries gets stuck in daemons"
+  ]
+
+prefix :: Text -> Text -> (Text -> Bool, Text)
+prefix part reason = ((part `T.isPrefixOf`), reason)
+
+infixOf :: Text -> Text -> (Text -> Bool, Text)
+infixOf part reason = ((part `T.isInfixOf`), reason)
+
+eq :: Text -> Text -> (Text -> Bool, Text)
+eq part reason = ((part ==), reason)
