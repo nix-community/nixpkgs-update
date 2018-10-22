7c42932b80c70a6981c098e1d001c26dd59d6343
Ryan Mulligan <ryan@ryantm.com>
2018-10-21 22:12:27 -0700

    make parser for nix-env query result line

--- src/Outpaths.hs
+++ src/Outpaths.hs
@@ -8,14 +8,21 @@ module Outpaths
 where
 
 import           Data.Semigroup ((<>))
+import           Data.Set
 import           Data.Text (Text)
 import qualified Data.Text as T
-import           NeatInterpolation (text)
+import           Data.Vector (Vector)
+import qualified Data.Vector as V
+import qualified NeatInterpolation (text)
 import           Shelly
+import qualified Text.Parsec (parse)
+import           Text.Parser.Char
+import           Text.Parser.Combinators
 import           Utils
+
 default (Text)
 
-outPathsExpr = [text|
+outPathsExpr = [NeatInterpolation.text|
 
 (let
   lib = import ./lib;
@@ -75,5 +82,52 @@ outPath :: Sh Text
 outPath = sub $ do
   Utils.setupNixpkgs
   cmd "curl" "-o" "outpaths.nix" "https://raw.githubusercontent.com/NixOS/ofborg/released/ofborg/src/outpaths.nix"
-  setenv "GC_INITIAL_HEAP_SIZE" "4g"
+  setenv "GC_INITIAL_HEAP_SIZE" "10g"
   cmd "nix-env" "-f" "./outpaths.nix" "-qaP" "--no-name" "--out-path" "--arg" "checkMeta" "true"
+
+data Outpath = Outpath
+  { mayName :: Maybe Text
+  , storePath :: Text
+  } deriving (Eq, Ord, Show)
+
+data ResultLine = ResultLine
+  { package :: Text
+  , architecture :: Text
+  , outpaths :: Vector Outpath
+  } deriving (Eq, Ord, Show)
+
+
+-- Example query result line:
+-- haskellPackages.agum.x86_64-darwin                                            doc=/nix/store/n526rc0pa5h0krdzsdni5agcpvcd3cb9-agum-2.7-doc;/nix/store/s59r75svbjm724q5iaprq4mln5k6wcr9-agum-2.7
+parse :: CharParsing m => m (Set ResultLine)
+parse = undefined
+
+parseResultLine :: CharParsing m =>  m ResultLine
+parseResultLine =
+  ResultLine <$>
+  parseAttrpath <*>
+  parseArchitecture <*
+  spaces <*>
+  parseOutpaths
+
+parseAttrpath :: CharParsing m => m Text
+parseAttrpath =
+  T.concat <$> many (try parseAttrpathPart)
+
+parseAttrpathPart :: CharParsing m => m Text
+parseAttrpathPart =
+  T.append <$>
+  (T.pack <$> many (noneOf ". ")) <*>
+  text "."
+
+parseArchitecture :: CharParsing m => m Text
+parseArchitecture = T.pack <$> many (noneOf " ")
+
+parseOutpaths :: CharParsing m => m (Vector Outpath)
+parseOutpaths = V.fromList <$> (parseOutpath `sepBy1` text ";")
+
+parseOutpath :: CharParsing m => m Outpath
+parseOutpath =
+  Outpath <$>
+  optional (try (T.pack <$> (many (noneOf "=") <* text "="))) <*>
+  (T.pack <$> many (noneOf ";"))
