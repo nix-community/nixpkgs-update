3950aa3f36b36048ee0f8dbb8ad51f6b880b1c5e
Ryan Mulligan <ryan@ryantm.com>
2019-09-28 09:36:22 -0700

    CVE,NVD: fix warnings

--- src/CVE.hs
+++ src/CVE.hs
@@ -10,50 +10,58 @@ module CVE
 import OurPrelude
 
 import Data.Aeson
-  (FromJSON, Object, eitherDecode, parseJSON, withObject, withText, (.:),
-  (.:!))
+  ( FromJSON
+  , Object
+  , (.:)
+  , (.:!)
+  , eitherDecode
+  , parseJSON
+  , withObject
+  , withText
+  )
 import Data.Aeson.Types (Parser)
 import Data.Bifunctor (bimap)
-import Data.Map (Map)
-import Data.Text.Read (decimal)
-import Data.Time.Clock (UTCTime)
-
-import Utils (Boundary(..), ProductID, Version, VersionMatcher(..))
-
 import qualified Data.ByteString.Lazy.Char8 as BSL
+import Data.Map (Map)
 import qualified Data.Map as M
 import qualified Data.Text as T
+import Data.Text.Read (decimal)
+import Data.Time.Clock (UTCTime)
+import Utils (Boundary(..), ProductID, VersionMatcher(..))
 
-data CVE = CVE
-  { cveID :: Text
-  , cveYear :: Int
-  , cveSubID :: Int
-  , cveAffects :: Map ProductID [VersionMatcher]
-  , cveDescriptions :: Text
-  , cveCPEs :: [CPE]
-  , cvePublished :: UTCTime
-  , cveLastModified :: UTCTime
-  } deriving (Show)
+data CVE =
+  CVE
+    { cveID :: Text
+    , cveYear :: Int
+    , cveSubID :: Int
+    , cveAffects :: Map ProductID [VersionMatcher]
+    , cveDescriptions :: Text
+    , cveCPEs :: [CPE]
+    , cvePublished :: UTCTime
+    , cveLastModified :: UTCTime
+    }
+  deriving (Show)
 
-data CPE = CPE
-  { cpePart :: Text
-  , cpeVendor :: Text
-  , cpeProduct :: Text
-  , cpeVersion :: Text
-  , cpeUpdate :: Text
-  , cpeEdition :: Text
-  , cpeLanguage :: Text
-  , cpeSoftwareEdition :: Text
-  , cpeTargetSoftware :: Text
-  , cpeTargetHardware :: Text
-  , cpeOther :: Text
-  } deriving (Show)
+data CPE =
+  CPE
+    { cpePart :: Text
+    , cpeVendor :: Text
+    , cpeProduct :: Text
+    , cpeVersion :: Text
+    , cpeUpdate :: Text
+    , cpeEdition :: Text
+    , cpeLanguage :: Text
+    , cpeSoftwareEdition :: Text
+    , cpeTargetSoftware :: Text
+    , cpeTargetHardware :: Text
+    , cpeOther :: Text
+    }
+  deriving (Show)
 
-type CVEID = Text
-type CVEIndex = Map ProductID [(VersionMatcher, [CVEID])]
-
+-- type CVEID = Text
+--type CVEIndex = Map ProductID [(VersionMatcher, [CVEID])]
 eitherToParser :: Either String a -> Parser a
-eitherToParser (Left e)  = fail e
+eitherToParser (Left e) = fail e
 eitherToParser (Right a) = pure a
 
 cveIDNumbers :: Text -> Either String (Int, Int)
@@ -68,75 +76,84 @@ cveIDNumbers rest0 = do
 parseDescriptions :: Object -> Parser Text
 parseDescriptions o = do
   dData <- o .: "description_data"
-  descriptions <- fmap concat $ sequence $ flip map dData $ \dDatum -> do
-    value <- dDatum .: "value"
-    lang :: Text <- dDatum .: "lang"
-    pure $ case lang of
-      "en" -> [value]
-      _    -> []
+  descriptions <-
+    fmap concat $
+    sequence $
+    flip map dData $ \dDatum -> do
+      value <- dDatum .: "value"
+      lang :: Text <- dDatum .: "lang"
+      pure $
+        case lang of
+          "en" -> [value]
+          _ -> []
   case descriptions of
     [d] -> pure d
     -- []  -> pure ""
-    _   -> fail "multiple english descriptions"
+    _ -> fail "multiple english descriptions"
 
-foobar :: (FromJSON a) => [a] -> (a -> Parser [b]) -> Parser [b]
-foobar l f = fmap concat $ sequence $ map f l
-
+-- foobar :: (FromJSON a) => [a] -> (a -> Parser [b]) -> Parser [b]
+-- foobar l f = fmap concat $ sequence $ map f l
 parseAffects :: Object -> Parser (Map ProductID [VersionMatcher])
 parseAffects o = do
   vendor <- o .: "vendor"
   vendorData <- vendor .: "vendor_data"
-  fmap (M.fromListWith (<>) . concat) $ sequence $ flip map vendorData $ \v -> do
-    product <- v .: "product"
-    productData <- product .: "product_data"
-    sequence $ flip map productData $ \p -> do
-      productID <- p .: "product_name"
-      version <- p .: "version"
-      versionData <- version .: "version_data"
-      matchers <- sequence $ flip map versionData $ \ver -> do
-        value <- ver .: "version_value"
-        affected :: Text <- ver .: "version_affected"
-        case affected of
-          "="  -> pure $ FuzzyMatcher value
-          "<=" -> pure $ RangeMatcher Unbounded (Including value)
-          _    -> fail $ "unknown version comparator: " <> show affected
-      pure (productID, matchers)
+  fmap (M.fromListWith (<>) . concat) $
+    sequence $
+    flip map vendorData $ \v -> do
+      productPart <- v .: "product"
+      productData <- productPart .: "product_data"
+      sequence $
+        flip map productData $ \p -> do
+          productID <- p .: "product_name"
+          version <- p .: "version"
+          versionData <- version .: "version_data"
+          matchers <-
+            sequence $
+            flip map versionData $ \ver -> do
+              value <- ver .: "version_value"
+              affected :: Text <- ver .: "version_affected"
+              case affected of
+                "=" -> pure $ FuzzyMatcher value
+                "<=" -> pure $ RangeMatcher Unbounded (Including value)
+                _ -> fail $ "unknown version comparator: " <> show affected
+          pure (productID, matchers)
 
 instance FromJSON CVE where
-  parseJSON = withObject "CVE" $ \o -> do
-    cve <- o .: "cve"
-    cfgs <- o .: "configurations"
-    cveCPEs <- parseConfigurations cfgs
-    meta <- cve .: "CVE_data_meta"
-    cveID <- meta .: "ID"
-    cvePublished <- o .: "publishedDate"
-    cveLastModified <- o .: "lastModifiedDate"
-    (cveYear, cveSubID) <- eitherToParser $ cveIDNumbers cveID
-    description <- cve .: "description"
-    cveDescriptions <- parseDescriptions description
-    affects <- cve .: "affects"
-    cveAffects <- parseAffects affects
-    pure CVE{..}
+  parseJSON =
+    withObject "CVE" $ \o -> do
+      cve <- o .: "cve"
+      cfgs <- o .: "configurations"
+      cveCPEs <- parseConfigurations cfgs
+      meta <- cve .: "CVE_data_meta"
+      cveID <- meta .: "ID"
+      cvePublished <- o .: "publishedDate"
+      cveLastModified <- o .: "lastModifiedDate"
+      (cveYear, cveSubID) <- eitherToParser $ cveIDNumbers cveID
+      description <- cve .: "description"
+      cveDescriptions <- parseDescriptions description
+      affects <- cve .: "affects"
+      cveAffects <- parseAffects affects
+      pure CVE {..}
 
 splitCPE :: Text -> [Text]
 splitCPE = map (T.replace "\a" ":") . T.splitOn ":" . T.replace "\\:" "\a"
 
 instance FromJSON CPE where
-  parseJSON = withText "CPE" $ \t -> do
-    case splitCPE t of
-      [ "cpe", "2.3", cpePart, cpeVendor, cpeProduct, cpeVersion, cpeUpdate
-        , cpeEdition, cpeLanguage, cpeSoftwareEdition, cpeTargetSoftware
-        , cpeTargetHardware, cpeOther
-        ] -> pure CPE{..}
-      _ -> fail $ "unparsable CPE: " <> T.unpack t
+  parseJSON =
+    withText "CPE" $ \t -> do
+      case splitCPE t of
+        ["cpe", "2.3", cpePart, cpeVendor, cpeProduct, cpeVersion, cpeUpdate, cpeEdition, cpeLanguage, cpeSoftwareEdition, cpeTargetSoftware, cpeTargetHardware, cpeOther] ->
+          pure CPE {..}
+        _ -> fail $ "unparsable CPE: " <> T.unpack t
 
 guardAttr :: (Eq a, FromJSON a, Show a) => Object -> Text -> a -> Parser ()
 guardAttr object attribute expected = do
   actual <- object .: attribute
-  unless (actual == expected) $ fail
-    $ "unexpected " <> T.unpack attribute
-      <> ", expected " <> show expected
-      <> ", got " <> show actual
+  unless (actual == expected) $
+    fail $
+    "unexpected " <>
+    T.unpack attribute <>
+    ", expected " <> show expected <> ", got " <> show actual
 
 -- TODO: Determine how nodes work exactly. What does AND mean and what does
 -- vulnerable: false mean? For now, we assume everything with vulnerable: true
@@ -149,11 +166,15 @@ parseNode node = do
 parseNode' :: (Maybe [Object]) -> Object -> Parser [CPE]
 parseNode' Nothing node = do
   matches <- node .: "cpe_match"
-  fmap concat $ sequence $ flip map matches $ \match -> do
-    vulnerable <- match .: "vulnerable"
-    cpe <- match .: "cpe23Uri"
-    pure $ if vulnerable then [cpe] else []
-
+  fmap concat $
+    sequence $
+    flip map matches $ \match -> do
+      vulnerable <- match .: "vulnerable"
+      cpe <- match .: "cpe23Uri"
+      pure $
+        if vulnerable
+          then [cpe]
+          else []
 parseNode' (Just children) _ = do
   fmap concat $ sequence $ map parseNode children
 
@@ -166,7 +187,10 @@ parseConfigurations o = do
 parseFeed :: BSL.ByteString -> Either Text [CVE]
 parseFeed = bimap T.pack cvefItems . eitherDecode
 
-data CVEFeed = CVEFeed { cvefItems :: [CVE] }
+data CVEFeed =
+  CVEFeed
+    { cvefItems :: [CVE]
+    }
 
 instance FromJSON CVEFeed where
   parseJSON = withObject "CVEFeed" $ \o -> CVEFeed <$> o .: "CVE_Items"
--- src/NVD.hs
+++ src/NVD.hs
@@ -4,38 +4,54 @@ module NVD where
 
 import OurPrelude
 
+import CVE (CVE, parseFeed)
 import Codec.Compression.GZip (decompress)
 import Control.Exception (ioError, try)
 import Crypto.Hash.SHA256 (hashlazy)
-import CVE (CVE, parseFeed)
 import Data.Bifunctor (second)
 import qualified Data.ByteString.Lazy.Char8 as BSL
 import Data.Hex (hex, unhex)
 import qualified Data.Text as T
 import Data.Time.Calendar (toGregorian)
 import Data.Time.Clock
-  (NominalDiffTime, UTCTime, diffUTCTime, getCurrentTime, nominalDay, utctDay)
+  ( NominalDiffTime
+  , UTCTime
+  , diffUTCTime
+  , getCurrentTime
+  , nominalDay
+  , utctDay
+  )
 import Data.Time.ISO8601 (parseISO8601)
 import Network.HTTP.Conduit (simpleHttp)
 import System.Directory
-  (XdgDirectory(..), createDirectoryIfMissing, getModificationTime,
-  getXdgDirectory)
+  ( XdgDirectory(..)
+  , createDirectoryIfMissing
+  , getModificationTime
+  , getXdgDirectory
+  )
 import System.FilePath ((<.>), (</>))
 import System.IO.Error (userError)
 import Utils (ProductID, Version)
 
 -- | Either @recent@, @modified@, or any year since @2002@.
 type FeedID = String
+
 type Extension = String
+
 type Timestamp = UTCTime
+
 type Checksum = BSL.ByteString
+
 type CompressedFeed = BSL.ByteString
+
 type MaxAge = NominalDiffTime
 
-data Meta = Meta Timestamp Checksum
+data Meta =
+  Meta Timestamp Checksum
 
 feedURL :: FeedID -> Extension -> String
-feedURL feed ext = "https://nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-" <> feed <> ext
+feedURL feed ext =
+  "https://nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-" <> feed <> ext
 
 throwString :: MonadIO m => String -> m a
 throwString = liftIO . ioError . userError
@@ -47,15 +63,18 @@ allYears :: IO [FeedID]
 allYears = do
   now <- liftIO getCurrentTime
   let (year, _, _) = toGregorian $ utctDay now
-  return $ map show [2002..year]
+  return $ map show [2002 .. year]
 
 parseMeta :: BSL.ByteString -> Either T.Text Meta
 parseMeta raw = do
-  let splitLine = second BSL.tail . BSL.break (==':') . BSL.takeWhile (/='\r')
+  let splitLine = second BSL.tail . BSL.break (== ':') . BSL.takeWhile (/= '\r')
   let fields = map splitLine $ BSL.lines raw
-  lastModifiedDate <- note "no lastModifiedDate in meta" $ lookup "lastModifiedDate" fields
+  lastModifiedDate <-
+    note "no lastModifiedDate in meta" $ lookup "lastModifiedDate" fields
   sha256 <- note "no sha256 in meta" $ lookup "sha256" fields
-  timestamp <- note "invalid lastModifiedDate in meta" $ parseISO8601 $ BSL.unpack lastModifiedDate
+  timestamp <-
+    note "invalid lastModifiedDate in meta" $
+    parseISO8601 $ BSL.unpack lastModifiedDate
   checksum <- note "invalid sha256 in meta" $ unhex sha256
   return $ Meta timestamp checksum
 
@@ -65,14 +84,15 @@ getMeta feed = do
   either throwText pure $ parseMeta raw
 
 getCVEs :: (ProductID, Version) -> IO [CVE]
-getCVEs (product, version) = do
+getCVEs (_product, _) = do
   years <- allYears
-  feeds <- sequence $ map (cacheFeed (7 * nominalDay)) years
+  _feeds <- sequence $ map (cacheFeed (7 * nominalDay)) years
   return []
 
 updateVulnDB :: IO ()
-updateVulnDB = do
+updateVulnDB
   -- This will be enough to develop with.
+ = do
   feed <- cacheFeed (99 * nominalDay) "2019"
   putStrLn $ "loading data"
   parsed <- either throwText pure $ parseFeed feed
@@ -82,7 +102,7 @@ updateVulnDB = do
   -- feeds <- sequence $ map (cacheFeed (7 * nominalDay)) years
   return ()
 
-getCacheFile ::  MonadIO m => FeedID -> m FilePath
+getCacheFile :: MonadIO m => FeedID -> m FilePath
 getCacheFile feed = do
   cacheDir <- liftIO $ getXdgDirectory XdgCache "nixpkgs-update/nvd"
   liftIO $ createDirectoryIfMissing True cacheDir
@@ -93,19 +113,21 @@ cacheFeed maxAge feed = do
   cacheFile <- getCacheFile feed
   cacheTime <- liftIO $ try $ getModificationTime cacheFile
   Meta newestTime expectedChecksum <- getMeta feed
-  let needsUpdate = case cacheTime of
-        Left (_ :: IOError) -> True
-        Right t -> diffUTCTime newestTime t > maxAge
-
+  let needsUpdate =
+        case cacheTime of
+          Left (_ :: IOError) -> True
+          Right t -> diffUTCTime newestTime t > maxAge
   if needsUpdate
     then do
       liftIO $ putStrLn $ "updating feed: " <> feed
       compressed <- simpleHttp $ feedURL feed ".json.gz"
       let raw = decompress compressed
       let actualChecksum = BSL.fromStrict $ hashlazy raw
-      when (actualChecksum /= expectedChecksum) $ throwString $
-        "wrong hash, expected: " <> BSL.unpack (hex expectedChecksum)
-        <> " got: " <> BSL.unpack (hex actualChecksum)
+      when (actualChecksum /= expectedChecksum) $
+        throwString $
+        "wrong hash, expected: " <>
+        BSL.unpack (hex expectedChecksum) <>
+        " got: " <> BSL.unpack (hex actualChecksum)
       liftIO $ BSL.writeFile cacheFile raw
       return raw
     else do
