29847728fb4a50af50495c6fa34c27a760529ba6
Ryan Mulligan <ryan@ryantm.com>
2019-11-24 14:39:52 -0800

    [CVE] fix uzbl issues

--- src/NVDRules.hs
+++ src/NVDRules.hs
@@ -3,9 +3,15 @@
 module NVDRules where
 
 import CVE (CPE(..), CPEMatch(..), CVE(..))
-import Utils (ProductID)
+import Utils (Boundary(..), ProductID, VersionMatcher(..))
 
 filter :: CVE -> CPEMatch -> ProductID -> Bool
+filter _ cpeMatch "uzbl"
+  | cpeMatchVersionMatcher cpeMatch ==
+      RangeMatcher Unbounded (Including "2009.12.22") = False
+filter _ cpeMatch "uzbl"
+  | cpeMatchVersionMatcher cpeMatch ==
+      RangeMatcher Unbounded (Including "2010.04.03") = False
 filter _ cpeMatch "terraform"
   | cpeTargetSoftware (cpeMatchCPE cpeMatch) == Just "aws" = False
 filter _cve _match _productID = True
