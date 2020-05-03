7f312dc1ff1c9cfe342f21776e1f160fd0ac4be2
Benjamin Hipple <bhipple@protonmail.com>
2020-05-03 12:05:21 -0400

    Hide section about NixPkgs review when it was skipped
    We're no longer running this on `staging` PRs, and users doing single-package
    updates have the option to omit `nixpkgs-review`, so we should only include the
    PR comment section if it was actually run.
    
    This continues to enhance the automated tests for the PR comment formatting,
    so you can see what the diff looks like just by looking at the test suite!

--- test/UpdateSpec.hs
+++ test/UpdateSpec.hs
@@ -10,27 +10,41 @@ import qualified Utils
 main :: IO ()
 main = hspec spec
 
+-- Tests here will write their actual results to an actual*.md file next to the
+-- expected one. If you are updating the PR description, run the test suite and
+-- copy all the actuals over to expected. This can be viewed with `git diff`
+-- then to ensure your changes look as expected.
 spec :: Spec
 spec = do
   describe "PR message" do
+    -- Common mock options
+    let options = Utils.Options False False "" False False False False
+    let updateEnv = Utils.UpdateEnv "foobar" "1.0" "1.1" (Just "https://update-site.com") options
+    let isBroken = False
+    let metaDescription = "\"Foobar package description\""
+    let metaHomepage = "\"https://foobar-homepage.com\""
+    let rewriteMsgs = ["Version Update", "Other Update"]
+    let releaseUrl = "https://github.com/foobar/releases"
+    let compareUrl = "https://github.com/foobar/compare"
+    let resultCheckReport = "- Some other check"
+    let commitHash = "af39cf77a0d42a4f6771043ec54221ed"
+    let attrPath = "foobar"
+    let maintainersCc = "@maintainer1"
+    let resultPath = "/nix/store/some-hash-path"
+    let opReport = "123 total rebuild path(s)"
+    let cveRep = ""
+    let cachixTestInstructions = ""
+    let nixpkgsReviewMsg = "nixpkgs-review comment body"
+
     it "matches a simple mock example" do
       expected <- T.readFile "test_data/expected_pr_description_1.md"
-      let options = Utils.Options False False "" False False False False
-      let updateEnv = Utils.UpdateEnv "foobar" "1.0" "1.1" (Just "https://update-site.com") options
-      let isBroken = False
-      let metaDescription = "\"Foobar package description\""
-      let metaHomepage = "\"https://foobar-homepage.com\""
-      let rewriteMsgs = ["Version Update", "Other Update"]
-      let releaseUrl = "https://github.com/foobar/releases"
-      let compareUrl = "https://github.com/foobar/compare"
-      let resultCheckReport = "- Some other check"
-      let commitHash = "af39cf77a0d42a4f6771043ec54221ed"
-      let attrPath = "foobar"
-      let maintainersCc = "@maintainer1"
-      let resultPath = "/nix/store/some-hash-path"
-      let opReport = "123 total rebuild path(s)"
-      let cveRep = ""
-      let cachixTestInstructions = ""
-      let nixpkgsReviewMsg = "nixpkgs-review comment body"
       let actual = Update.prMessage updateEnv isBroken metaDescription metaHomepage rewriteMsgs releaseUrl compareUrl resultCheckReport commitHash attrPath maintainersCc resultPath opReport cveRep cachixTestInstructions nixpkgsReviewMsg
+      T.writeFile "test_data/actual_pr_description_1.md" actual
+      actual `shouldBe` expected
+
+    it "does not include NixPkgs review section when no review was done" do
+      expected <- T.readFile "test_data/expected_pr_description_2.md"
+      let nixpkgsReviewMsg' = ""
+      let actual = Update.prMessage updateEnv isBroken metaDescription metaHomepage rewriteMsgs releaseUrl compareUrl resultCheckReport commitHash attrPath maintainersCc resultPath opReport cveRep cachixTestInstructions nixpkgsReviewMsg'
+      T.writeFile "test_data/actual_pr_description_2.md" actual
       actual `shouldBe` expected
