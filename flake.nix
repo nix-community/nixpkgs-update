29575b3a388dee17f2e4a7ce2d1a7d7f6d135706
Ryan Mulligan <ryan@ryantm.com>
2020-10-18 09:31:10 -0700

    parameterize flake.nix over systems

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "A flake for nixpkgs-update";
 
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
   inputs.flake-compat = {
     url = "github:edolstra/flake-compat";
     flake = false;
@@ -19,50 +21,51 @@
     flake = false;
   };
 
-  outputs = { self, flake-compat, nixpkgs, nixpkgs-review }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        config = { allowBroken = true; };
-      };
+  outputs = { self, flake-utils, flake-compat, nixpkgs, nixpkgs-review }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { allowBroken = true; };
+        };
 
-      compiler = pkgs.haskell.packages.ghc884;
+        compiler = pkgs.haskell.packages.ghc884;
 
-      developPackageAttrs = {
-        name = "nixpkgs-update";
-        root = self;
-        overrides = self: super: { };
-        source-overrides = { };
-        modifier = pkgs.haskell.lib.disableLibraryProfiling;
-        returnShellEnv = false;
-      };
+        developPackageAttrs = {
+          name = "nixpkgs-update";
+          root = self;
+          overrides = self: super: { };
+          source-overrides = { };
+          modifier = pkgs.haskell.lib.disableLibraryProfiling;
+          returnShellEnv = false;
+        };
 
-      developPackageShellAttrs = developPackageAttrs // {
-        returnShellEnv = true;
-      };
+        developPackageShellAttrs = developPackageAttrs // {
+          returnShellEnv = true;
+        };
 
-      drvAttrs = attrs: with pkgs; {
-        # TODO: lock down coreutils paths too
-        NIX = nix;
-        GIT = git;
-        HUB = gitAndTools.hub;
-        JQ = jq;
-        TREE = tree;
-        GIST = gist;
-        NIXPKGSREVIEW = (import nixpkgs-review { inherit pkgs; });
-      };
+        drvAttrs = attrs: with pkgs; {
+          # TODO: lock down coreutils paths too
+          NIX = nix;
+          GIT = git;
+          HUB = gitAndTools.hub;
+          JQ = jq;
+          TREE = tree;
+          GIST = gist;
+          NIXPKGSREVIEW = (import nixpkgs-review { inherit pkgs; });
+        };
 
-      drvShellAttrs = attrs: (drvAttrs attrs) // (with pkgs; {
-        nativeBuildInputs = attrs.nativeBuildInputs ++ [ cabal-install ghcid ];
+        drvShellAttrs = attrs: (drvAttrs attrs) // (with pkgs; {
+          nativeBuildInputs = attrs.nativeBuildInputs ++ [ cabal-install ghcid ];
+        });
+
+        pkg = (compiler.developPackage developPackageAttrs).overrideAttrs drvAttrs;
+
+        shell = (compiler.developPackage developPackageShellAttrs).overrideAttrs drvShellAttrs;
+
+      in {
+        devShell = shell;
+        packages.nixpkgs-update = pkg;
+        defaultPackage = self.packages."${system}".nixpkgs-update;
       });
-
-      pkg = (compiler.developPackage developPackageAttrs).overrideAttrs drvAttrs;
-
-      shell = (compiler.developPackage developPackageShellAttrs).overrideAttrs drvShellAttrs;
-
-    in {
-      devShell = shell;
-      packages.x86_64-linux.nixpkgs-update = pkg;
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.nixpkgs-update;
-    };
 }
