d2ead5f8219d6e2bb645ea67516c4c89d607df49
l198881 <82917144+l198881@users.noreply.github.com>
2021-06-01 08:26:13 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,37 +1,37 @@
 {
-  description = "A flake for nixpkgs-update";
+  description  "A flake for nixpkgs-update";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs-review.url = "github:mic92/nixpkgs-review";
-  inputs.flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
-  inputs.nixpkgs = { type = "github"; owner = "nixos"; repo = "nixpkgs"; };
+  inputs.flake-utils.url  "github:numtide/flake-utils";
+  inputs.nixpkgs-review.url  "github:mic92/nixpkgs-review";
+  inputs.flake-compat  { url  "github:edolstra/flake-compat"; flake = false; };
+  inputs.nixpkgs  { type  "github"; owner = "nixos"; repo = "nixpkgs"; };
 
-  outputs = { self, flake-utils, flake-compat, nixpkgs, nixpkgs-review }:
+  outputs  { self, flake-utils, flake-compat, nixpkgs, nixpkgs-review }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
       let
-        pkgs = import nixpkgs { inherit system; config = { allowBroken = true; }; };
+        pkgs =  nixpkgs {  system; config = { allowBroken = true; }; };
 
         developPackageAttrs = {
-          name = "nixpkgs-update";
-          root = self;
-          returnShellEnv = false;
+          name  "nixpkgs-update";
+          root  self;
+          returnShellEnv  false;
         };
 
-        drvAttrs = attrs: with pkgs; {
-          NIX = nix;
-          GIT = git;
-          HUB = gitAndTools.hub;
-          JQ = jq;
-          TREE = tree;
-          GIST = gist;
+        drvAttrs  attrs: pkgs; {
+          NIX  nix;
+          GIT  git;
+          HUB  gitAndTools.hub;
+          JQ  jq;
+          TREE  tree;
+          GIST  gist;
           # TODO: are there more coreutils paths that need locking down?
-          TIMEOUT = coreutils;
-          NIXPKGSREVIEW = (import nixpkgs-review { inherit pkgs; });
+          TIMEOUT  coreutils;
+          NIXPKGSREVIEW  (import nixpkgs-review { inherit pkgs; });
         };
 
-        haskellPackages = pkgs.haskell.packages.ghc8102.override {
-          overrides = _: haskellPackages: {
-            nixpkgs-update =
+        haskellPackages  pkgs.haskell.packages.ghc8102.override {
+          overrides  _: haskellPackages: {
+            nixpkgs-update 
               pkgs.haskell.lib.justStaticExecutables (
                 pkgs.haskell.lib.failOnAllWarnings (
                   pkgs.haskell.lib.disableExecutableProfiling (
@@ -46,20 +46,20 @@
           };
         };
 
-        shell = haskellPackages.shellFor {
-          nativeBuildInputs = with pkgs; [
+        shell  haskellPackages.shellFor {
+          nativeBuildInputs  pkgs; [
             cabal-install
             ghcid
           ];
-          packages = ps: [ ps.nixpkgs-update ];
-          shellHook = ''
+          packages  ps: [ ps.nixpkgs-update ];
+          shellHook  ''
           '';
         };
 
       in
       {
-        devShell = shell;
-        packages.nixpkgs-update = haskellPackages.nixpkgs-update;
-        defaultPackage = haskellPackages.nixpkgs-update;
+        devShell  shell;
+        packages.nixpkgs-update  haskellPackages.nixpkgs-update;
+        defaultPackage  haskellPackages.nixpkgs-update;
       });
 }
