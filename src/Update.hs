503f7b0d94b6eb780a51823972e6ab0204b0a137
Scott Edlund <sedlund@users.noreply.github.com>
2024-08-06 20:35:27 +0200

    prMessage: suggest :+1: reaction to important pr (#436)

--- src/Update.hs
+++ src/Update.hs
@@ -606,6 +606,13 @@ prMessage updateEnv isBroken metaDescription metaHomepage metaChangelog rewriteM
        ###### Maintainer pings
 
        $maintainersCc
+
+       ---
+
+       Add a :+1: [reaction] to [pull requests you find important].
+
+       [reaction]: https://github.blog/2016-03-10-add-reactions-to-pull-requests-issues-and-comments/
+       [pull requests you find important]: https://github.com/NixOS/nixpkgs/pulls?q=is%3Aopen+sort%3Areactions-%2B1-desc
     |]
 
 jqBin :: String
